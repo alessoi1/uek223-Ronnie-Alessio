72a4bf8e845f2e16e0233094c2b1a27762f0c704
alessoi1 <69907003+alessoi1@users.noreply.github.com>
2022-02-23 16:14:27 +0100

    Add Dependency for annotations

--- uk223-main/src/main/java/com/example/demo/domain/appMyListEntrry/MyListEntry.java
+++ uk223-main/src/main/java/com/example/demo/domain/appMyListEntrry/MyListEntry.java
@@ -8,10 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
