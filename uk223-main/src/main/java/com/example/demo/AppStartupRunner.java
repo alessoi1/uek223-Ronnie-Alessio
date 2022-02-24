c9e893ef7e4f565b0fa8c26314938a106b7c1672
noserronnie <93775605+noserronnie@users.noreply.github.com>
2022-02-24 13:30:52 +0100

    Log4J implementiert

--- uk223-main/src/main/java/com/example/demo/AppStartupRunner.java
+++ uk223-main/src/main/java/com/example/demo/AppStartupRunner.java
@@ -1,26 +1,23 @@
 package com.example.demo;
+
 import com.example.demo.domain.appUser.User;
 import com.example.demo.domain.appUser.UserService;
 import com.example.demo.domain.authority.Authority;
 import com.example.demo.domain.authority.AuthorityRepository;
 import com.example.demo.domain.role.Role;
 import com.example.demo.domain.role.RoleRepository;
-import com.example.demo.domain.role.RoleServiceImpl;
 import lombok.RequiredArgsConstructor;
-
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Set;
-
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 //ApplicationListener used to run commands after startup
 class AppStartupRunner implements ApplicationRunner {
     @Autowired
