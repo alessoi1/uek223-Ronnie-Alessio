60c0a3caa99f7582de6c4c4753e6c30b252f95a8
noserronnie <93775605+noserronnie@users.noreply.github.com>
2022-02-24 11:51:30 +0100

    Fixed Custom Query and Show SQL

--- uk223-main/src/main/java/com/example/demo/domain/appMyListEntrry/MyListEntryRepository.java
+++ uk223-main/src/main/java/com/example/demo/domain/appMyListEntrry/MyListEntryRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.appMyListEntrry;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import java.util.UUID;
 
 @Repository
 public interface MyListEntryRepository extends JpaRepository<MyListEntry, UUID> {
-    @Query("SELECT m FROM mylistentry m WHERE m.user.id = :#{#userId} ORDER BY m.wichtigkeit")
+    @Modifying
+    @Query("SELECT m FROM mylistentry m WHERE m.user.id = :userId ORDER BY m.wichtigkeit")
     List<MyListEntry> findAllByUser(@Param("userId") UUID userId);
 }
