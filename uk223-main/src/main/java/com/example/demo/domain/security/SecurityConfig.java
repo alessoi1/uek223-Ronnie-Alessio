ae308a82a7602feeb0c2129899cfdcd86701a2af
noserronnie <93775605+noserronnie@users.noreply.github.com>
2022-02-25 14:25:37 +0100

    Passwort Verschlüsselung
    Passwörter werden jetzt verschlüsselt.
    Ausserdem werden die Testdaten nicht mehr im data.sql sondern im AppStartupRunner.java erstellt.

--- uk223-main/src/main/java/com/example/demo/domain/security/SecurityConfig.java
+++ uk223-main/src/main/java/com/example/demo/domain/security/SecurityConfig.java
@@ -3,12 +3,15 @@ package com.example.demo.domain.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
  @EnableWebSecurity @RequiredArgsConstructor @EnableGlobalMethodSecurity(prePostEnabled = true)
 
@@ -17,17 +20,29 @@ import org.springframework.security.crypto.password.PasswordEncoder;
      private final UserDetailsService userDetailsService;
      private final PasswordEncoder passwordEncoder;
 
+     @Bean
+     public PasswordEncoder encoder() {
+         return new BCryptPasswordEncoder(4);
+     }
+
+     @Bean
+     public DaoAuthenticationProvider authProvider() {
+         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+         authProvider.setUserDetailsService(userDetailsService);
+         authProvider.setPasswordEncoder(encoder());
+         return authProvider;
+     }
+
      @Override
      protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-         auth.userDetailsService(userDetailsService);
-
+         auth.userDetailsService(userDetailsService).passwordEncoder(encoder()).and()
+                 .authenticationProvider(authProvider());
      }
 
      @Autowired
      public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
          auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
      }
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
