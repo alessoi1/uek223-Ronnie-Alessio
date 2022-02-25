b3d2a6ded9819dbf3e13ba215c0a5ec738d4a72f
noserronnie <93775605+noserronnie@users.noreply.github.com>
2022-02-25 09:51:37 +0100

    Update SecurityConfig.java

--- uk223-main/src/main/java/com/example/demo/domain/security/SecurityConfig.java
+++ uk223-main/src/main/java/com/example/demo/domain/security/SecurityConfig.java
@@ -31,7 +31,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-         http.csrf().disable();
+
+        http.csrf().disable();
 
         http.httpBasic().and()
                 .authorizeRequests()
