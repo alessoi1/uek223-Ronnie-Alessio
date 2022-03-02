85aa7ac606e200bdeb0eabf1c0f1874a5c7e410c
noserronnie <ronnie.hartmann@noser.com>
2022-03-02 14:30:33 +0100

    @Operation Annotations hinzugefügt

--- uk223-main/src/main/java/com/example/demo/domain/appUser/UserController.java
+++ uk223-main/src/main/java/com/example/demo/domain/appUser/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.appUser;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +19,13 @@ public class UserController {
 //    ADD YOUR ENDPOINT MAPPINGS HERE
 private final UserService userService;
 
+    @Operation(summary = "Get all Users")
     @GetMapping()
     public ResponseEntity<Collection<User>> findAll() {
         return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get single User by ID")
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable UUID id) {
         try {
@@ -32,6 +35,7 @@ private final UserService userService;
         }
     }
 
+    @Operation(summary = "Create new User (Register)")
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) {
         try {
